071f2a9a48b374fad266e84b61ed1346b9974f7a
Tragedia <Tragedia@LAPTOP-9QICE5TG>
2025-05-03 10:02:17 +0200

    acabo de descubrir una DAL que me hace falta

--- DTO/clsTemperaturaConNombreInvernadero.cs
+++ DTO/clsTemperaturaConNombreInvernadero.cs
@@ -21,9 +21,9 @@ namespace DTO
 
         }
 
-        public clsTemperaturaConNombreInvernadero(clsTemperatura temperatura, string nombreInvernadero) : base(temperatura.)
-
-            {
+        public clsTemperaturaConNombreInvernadero(clsTemperatura temperatura, string nombreInvernadero)
+            : base(temperatura.IdInvernadero, temperatura.Fecha, temperatura.Temp1, temperatura.Temp2, temperatura.Temp3, temperatura.Humedad1, temperatura.Humedad2, temperatura.Humedad3)
+        {
             this.nombreInvernadero = nombreInvernadero;
         }
     }
