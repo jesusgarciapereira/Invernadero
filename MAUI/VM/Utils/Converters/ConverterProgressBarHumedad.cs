b8f3383818faba4d94f0675f1968bb92d3829ad1
jgarcia <jgarcia@107-04>
2025-05-12 13:40:49 +0200

    Falta mejorar MAUI con los converter

---
+++ MAUI/VM/Utils/Converters/ConverterProgressBarHumedad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.VM.Utils.Converters
+{
+    public class ConverterProgressBarHumedad : IValueConverter
+    {
+        /// <summary>
+        /// Función que recive y convierte un valor entre 0 y 100 y devuelve un float entre 0 y 1
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            double humedad = 0;
+
+            if (value != null)
+            {
+                humedad = (double)value / 100.0;
+            }
+            return humedad;
+        }
+
+        /// <summary>
+        /// Retornamos el mismo valor porque no necesitamos ninguna conversión de vuelta
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
--- MAUI/VM/clsInvernaderoSeleccionadoVM.cs
+++ MAUI/VM/clsInvernaderoSeleccionadoVM.cs
@@ -15,9 +15,9 @@ namespace MAUI.VM
         private double progessTemp1;
         private double progessTemp2;
         private double progessTemp3;
-        private double progessHumedad1;
-        private double progessHumedad2;
-        private double progessHumedad3;
+        //private double progessHumedad1;
+        //private double progessHumedad2;
+        //private double progessHumedad3;
         #endregion
 
         #region Propiedades
@@ -38,18 +38,18 @@ namespace MAUI.VM
         {
             get { return progessTemp3; }
         }
-        public double ProgessHumedad1
-        {
-            get { return progessHumedad1; }
-        }
-        public double ProgessHumedad2
-        {
-            get { return progessHumedad2; }
-        }
-        public double ProgessHumedad3
-        {
-            get { return progessHumedad3; }
-        }
+        //public double ProgessHumedad1
+        //{
+        //    get { return progessHumedad1; }
+        //}
+        //public double ProgessHumedad2
+        //{
+        //    get { return progessHumedad2; }
+        //}
+        //public double ProgessHumedad3
+        //{
+        //    get { return progessHumedad3; }
+        //}
         #endregion
 
         #region Constructores
@@ -78,20 +78,20 @@ namespace MAUI.VM
                 this.progessTemp3 = (double)temperaturasDeInvernadero.Temp3 / 50.0;
             }
 
-            if (temperaturasDeInvernadero.Humedad1 != null)
-            {
-                this.progessHumedad1 = (double)temperaturasDeInvernadero.Humedad1 / 100.0;
-            }
+            //if (temperaturasDeInvernadero.Humedad1 != null)
+            //{
+            //    this.progessHumedad1 = (double)temperaturasDeInvernadero.Humedad1 / 100.0;
+            //}
 
-            if (temperaturasDeInvernadero.Humedad2 != null)
-            {
-                this.progessHumedad2 = (double)temperaturasDeInvernadero.Humedad2 / 100.0;
-            }
+            //if (temperaturasDeInvernadero.Humedad2 != null)
+            //{
+            //    this.progessHumedad2 = (double)temperaturasDeInvernadero.Humedad2 / 100.0;
+            //}
 
-            if (temperaturasDeInvernadero.Humedad3 != null)
-            {
-                this.progessHumedad3 = (double)temperaturasDeInvernadero.Humedad3 / 100.0;
-            }
+            //if (temperaturasDeInvernadero.Humedad3 != null)
+            //{
+            //    this.progessHumedad3 = (double)temperaturasDeInvernadero.Humedad3 / 100.0;
+            //}
         }
         #endregion
     }
